d23e2da8651f1c45dad96bebd489b4a40859672e
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-06-07 20:10:24 -0500

    Add + Test Manager class

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,20 @@
-Manager.test.js
+const Manager = require('../lib/Manager');
+
+describe('Employee', () => {
+    describe('Initialization', () => {
+        it('should return object values', () => {
+
+            const name = 'Zeus';
+            const ID = 4;
+            const email = 'lightning@gmail.com';
+            const officeNumber = 10;
+
+            const mangrObj = new Manager('Zeus', 4, 'lightning@gmail.com', 10);
+
+            expect(mangrObj.getName()).toEqual(name);
+            expect(mangrObj.getID()).toEqual(ID);
+            expect(mangrObj.getEmail()).toEqual(email);
+            expect(mangrObj.getOffice()).toEqual(officeNumber);
+        });
+    });
+});

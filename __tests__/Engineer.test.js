a3c9bb28df489c0e7171093ec0b561cbc77b5a8e
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-06-07 20:09:50 -0500

    Add + Test Engineer class

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,20 @@
-Engineer.test.js
+const Engineer = require('../lib/Engineer');
+
+describe('Employee', () => {
+    describe('Initialization', () => {
+        it('should return object values', () => {
+
+            const name = 'Davy Jones';
+            const ID = 2;
+            const email = 'djLocker@gmail.com';
+            const github = 'flyingDutchman';
+
+            const engObj = new Engineer('Davy Jones', 2, 'djLocker@gmail.com', 'flyingDutchman');
+
+            expect(engObj.getName()).toEqual(name);
+            expect(engObj.getID()).toEqual(ID);
+            expect(engObj.getEmail()).toEqual(email);
+            expect(engObj.getGithub()).toEqual(github);
+        });
+    });
+});

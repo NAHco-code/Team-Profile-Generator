7ebf28fa51dcb5325dc3383386045777e738138e
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-06-07 20:10:57 -0500

    Add + Test Intern class

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,20 @@
-Intern.test.js
+const Intern = require('../lib/Intern');
+
+describe('Employee', () => {
+    describe('Initialization', () => {
+        it('should return object values', () => {
+
+            const name = 'Hermes';
+            const ID = 3;
+            const email = 'messenger@gmail.com';
+            const school = 'Odyssey'
+
+            const intObj = new Intern('Hermes', 3, 'messenger@gmail.com', 'Odyssey');
+
+            expect(intObj.getName()).toEqual(name);
+            expect(intObj.getID()).toEqual(ID);
+            expect(intObj.getEmail()).toEqual(email);
+            expect(intObj.getSchool()).toEqual(school);
+        });
+    });
+});

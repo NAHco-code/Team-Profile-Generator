13358ffea8eb10d1075673e9b2b26670bb1dc418
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-06-07 20:08:50 -0500

    Add + Test Employee class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,19 @@
-Employee.test.js
+const Employee = require('../lib/Employee');
+const Manager = require('../lib/Manager');
+
+describe('Employee', () => {
+    describe('Initialization', () => {
+        it('should return object values', () => {
+
+            const name = 'Calypso';
+            const ID = 1;
+            const email = 'Calypso@gmail.com';
+
+            const empObj = new Employee('Calypso', 1, 'Calypso@gmail.com');
+
+            expect(empObj.getName()).toEqual(name);
+            expect(empObj.getID()).toEqual(ID);
+            expect(empObj.getEmail()).toEqual(email);
+        });
+    });
+});
